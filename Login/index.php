dab35d68381a5fa2809428738e5bc5f8285bf3cc
fabian-coder-segura <79112943+fabian-coder-segura@users.noreply.github.com>
2021-10-27 18:10:45 -0500

    login

---
+++ Login/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action = "validar.php" method= "post">
+        <h1>Sistema de Login</h1>
+        <p>Usuario <input type="text" placeholder="ingrese nu nombre" name="usuario"></p>
+        <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name ="contraseña"></p>
+        <input type="submit" value ="ingresar">
+
+
+    </form>
+    
+</body>
+</html>
